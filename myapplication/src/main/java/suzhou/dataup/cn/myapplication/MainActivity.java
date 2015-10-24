30dd1c24b9d5e4a58d4359654244cdce098aa0d5
liujingyuan <906514731@qq.com>
2015-10-24 19:12:37 +0800

    提交空格

--- myapplication/src/main/java/suzhou/dataup/cn/myapplication/MainActivity.java
+++ myapplication/src/main/java/suzhou/dataup/cn/myapplication/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
