f64f0ca945ee42d7aec259ef23e3326f7fb04b9a
liujingyuan <906514731@qq.com>
2015-11-10 17:40:49 +0800

    增加rxjva测试

--- myapplication/src/main/java/suzhou/dataup/cn/myapplication/MainActivity.java
+++ myapplication/src/main/java/suzhou/dataup/cn/myapplication/MainActivity.java
@@ -18,7 +18,12 @@ import android.view.MenuItem;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 import suzhou.dataup.cn.myapplication.adputer.SimpleFragmentPagerAdapter;
+import suzhou.dataup.cn.myapplication.utiles.LogUtil;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     @InjectView(R.id.toolbar)
@@ -49,6 +54,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toolbar.setTitle("简约");
         toolbar.setTitleTextColor(Color.WHITE);
         initCount();
+        initObse();
+
+    }
+
+    private void initObse() {
+        //目前不知道原因，如果这里不写成链式结构设置在子线程无效！
+        Observable<String> stringObservable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello, world!");
+                subscriber.onCompleted();
+                LogUtil.e("mySubscribersssss" + Thread.currentThread().getName());
+            }
+            //设置运行执行逻辑的时候在Io线程可以执行耗时的操作,回显结果在主线程！
+        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+        Subscriber<String> mySubscriber = new Subscriber<String>() {
+            @Override
+            public void onNext(String s) {
+                System.out.println(s);
+                LogUtil.e("mySubscriber" + s + Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onCompleted() {
+            }
+
+            @Override
+            public void onError(Throwable e) {
+            }
+        };
+        //myObservable订阅mySubscriber
+        stringObservable.subscribe(mySubscriber);
     }
 
     private void initCount() {
